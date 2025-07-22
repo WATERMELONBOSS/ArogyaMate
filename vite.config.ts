e37b477685c98d05e23261688b156334b1bc0a04
Milan Srinivas <milansrinivas.com>
2025-07-22 15:09:40 +0530

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ArogyaMate/", // 👈 Add this line
+ base: "/",
   server: {
     host: "::",
     port: 8080,
